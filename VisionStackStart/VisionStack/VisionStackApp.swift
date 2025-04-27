2dda34c7d3ee40369520b7738d681f026168184e
kkchow333 <chow1kaitlin@gmail.com>
2025-04-27 14:43:24 -0700

    base code

---
+++ VisionStackStart/VisionStack/VisionStackApp.swift
@@ -0,0 +1,18 @@
+//
+//  VisionStackApp.swift
+//  VisionStack
+//
+//  Created by Brian Advent on 23.03.24.
+//
+
+import SwiftUI
+
+@main
+struct VisionStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+    }
+}
